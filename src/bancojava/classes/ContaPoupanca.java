e2fa54bbc4483a448452a8edca802bd49b871786
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2022-10-22 17:57:34 -0300

    Teste Inicial

---
+++ src/bancojava/classes/ContaPoupanca.java
@@ -0,0 +1,18 @@
+package bancojava.classes;
+
+public class ContaPoupanca extends Conta{
+    private double rendimento;
+
+    public ContaPoupanca(Cliente Cliente) {
+        super(Cliente);
+
+
+    }
+
+    public void setRendimento(double rendimento) {
+        this.rendimento = rendimento;
+    }
+
+    public double getRendimento() {
+        return rendimento;
+}}
